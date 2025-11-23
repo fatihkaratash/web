03bfb27cf6656cbeb78dbb55c1a14da9523da726
fatihkaratash <f.karatash.dev@gmail.com>
2025-11-23 12:14:32 +0300

    feat: .env ve .gitignore eklendi

--- FitMindAI/Migrations/ApplicationDbContextModelSnapshot.cs
+++ FitMindAI/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -3,20 +3,17 @@ using System;
 using FitMindAI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace FitMindAI.Data.Migrations
+namespace FitMindAI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20251123083505_InitialCreate")]
-    partial class InitialCreate
+    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
     {
-        /// <inheritdoc />
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
