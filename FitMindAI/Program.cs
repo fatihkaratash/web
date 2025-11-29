fff684ee3f0eedd2a5b7c6d8b8823bc84b4a8b7a
fatihkaratash <f.karatash.dev@gmail.com>
2025-11-29 17:29:06 +0300

    feat: veritabanı güncellemeleri

--- FitMindAI/Program.cs
+++ FitMindAI/Program.cs
@@ -6,8 +6,7 @@ using FitMindAI.Services;
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
+//container
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
@@ -15,7 +14,6 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => {
     options.SignIn.RequireConfirmedAccount = false;
-    // Şifre kuralları: minimum 6 karakter, en az 1 rakam gerekli
     options.Password.RequireDigit = true;
     options.Password.RequireLowercase = true;
     options.Password.RequireUppercase = false;
@@ -38,7 +36,7 @@ builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Seed veritabanı (roller ve admin kullanıcısı)
+// (roller ve admin kullanıcısı)
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
@@ -54,7 +52,7 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  
     app.UseHsts();
 }
 
